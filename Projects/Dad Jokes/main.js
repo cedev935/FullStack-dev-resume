c5b35d4c0f0f109c9dd0c27aaeb7327f1a3b0e4d
Count&on&me <140622562+cedev935@users.noreply.github.com>
2023-09-13 15:19:33 -0400

    Added full stack developer resume.

---
+++ Projects/Dad Jokes/main.js
@@ -0,0 +1,33 @@
+const jokeBtn = document.getElementById('jokeBtn')
+const jokeEl =  document.getElementById('joke')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+async function generateJoke(){
+    const config = {
+        headers:{
+            'Accept':'application/json'
+        },
+            
+    }
+    const res = await fetch('https://icanhazdadjoke.com',config)
+    const data = await res.json()
+    jokeEl.innerHTML = data.joke
+}
+
+// USING .then
+// function generateJoke(){
+//     const config = {
+//         headers:{
+//             'Accept':'application/json'
+//         }
+            
+//     }
+//     fetch('https://icanhazdadjoke.com',config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//         jokeEl.innerHTML = data.joke
+//     })
+// }
---
+++ Projects/Keycode/main.js
@@ -0,0 +1,16 @@
+const insert = document.getElementById('insert')
+window.addEventListener('keydown', (e) =>{
+    insert.innerHTML = ` 
+    <div class="key">
+    ${event.key === '  ' ? 'Space' : event.key}
+    <small>event.key</small>
+</div>
+<div class="key">
+    ${event.keyCode}
+    <small>event.keyCode</small>
+</div>
+<div class="key">
+    ${event.code}
+    <small>event.code</small>
+</div>`
+})
